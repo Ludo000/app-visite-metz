514aebb2a7cc848ab0cb96aa7def116581f9921c
Ludovic Scholz <ludovicscholz@gmail.com>
2018-12-12 10:41:22 +0100

    SyncFragment

---
+++ app/src/main/java/com/example/sami/visitmetz_v2/SyncButtonUploadListener.java
@@ -0,0 +1,17 @@
+package com.example.sami.visitmetz_v2;
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+public class SyncButtonListener implements View.OnClickListener {
+    private Fragment syncFragment;
+
+    public SyncButtonListener(Fragment syncFragment){
+        this.syncFragment=syncFragment;
+
+    }
+    @Override
+    public void onClick(View v) {
+
+    }
+}

1687d75ec48d231fdebd96b7e85400766bde823e
Ludovic Scholz <ludovicscholz@gmail.com>
2018-12-12 10:50:43 +0100

    sync fragment 2

--- app/src/main/java/com/example/sami/visitmetz_v2/SyncButtonUploadListener.java
+++ app/src/main/java/com/example/sami/visitmetz_v2/SyncButtonUploadListener.java
@@ -1,17 +1,25 @@
 package com.example.sami.visitmetz_v2;
 
-import android.support.v4.app.Fragment;
 import android.view.View;
 
-public class SyncButtonListener implements View.OnClickListener {
-    private Fragment syncFragment;
+public class SyncButtonUploadListener implements View.OnClickListener {
+    public SyncFragment syncFragment;
 
-    public SyncButtonListener(Fragment syncFragment){
+    public SyncButtonUploadListener(SyncFragment syncFragment){
         this.syncFragment=syncFragment;
 
     }
     @Override
     public void onClick(View v) {
+        this.syncFragment.buttonDownload.setEnabled(false);
+        this.syncFragment.buttonUpload.setEnabled(false);
+        String currentText = this.syncFragment.textSyncOutput.getText().toString();
+        this.syncFragment.textSyncOutput.setText(currentText + "Téléversement de la base de données en cours...\n");
 
+        //Traitement
+
+
+        this.syncFragment.buttonDownload.setEnabled(true);
+        this.syncFragment.buttonUpload.setEnabled(true);
     }
 }
--- app/src/main/java/com/example/sami/visitmetz_v2/SyncFragment.java
+++ app/src/main/java/com/example/sami/visitmetz_v2/SyncFragment.java
@@ -1,13 +1,39 @@
 package com.example.sami.visitmetz_v2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class SyncActivity extends AppCompatActivity {
+public class SyncFragment extends Fragment {
+    public Button buttonUpload;
+    public Button buttonDownload;
+    public TextView textSyncOutput;
+    public SyncButtonUploadListener syncButtonUploadListener;
+    public SyncButtonDownloadListener syncButtonDownloadListener;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sync);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sync, container, false);
+        this.buttonDownload = view.findViewById(R.id.buttonDownload);
+        this.buttonUpload = view.findViewById(R.id.buttonUpload);
+        this.textSyncOutput = view.findViewById(R.id.textSyncOutput);
+        this.syncButtonUploadListener = new SyncButtonUploadListener(this);
+        this.syncButtonDownloadListener = new SyncButtonDownloadListener(this);
+        this.buttonUpload.setOnClickListener(this.syncButtonUploadListener);
+        this.buttonDownload.setOnClickListener(this.syncButtonDownloadListener);
+
+        return view;
     }
 }
